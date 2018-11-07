4a3852ff3662adfced7a04f0570192180cfd667e
v1leftw <dmitry.g.fake@gmail.com>
2018-11-07 20:12:13 +0300

    [+] task solution

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+Script searches users who sent solution to task after midnight.
+Script uses [API](https://devman.org/api/challenges/solution_attempts/) to get users.
+
+# Quickstart
+Examples of script launch on Linux, Python 3.5
+
+```
+$ python3 seek_dev_nighters.py
+Searching midnighters...
+Midnighters:
+michael.bro.18
+illarionovanton
+OlegPrikhodko
+vpforwork15
+ruslansv
+julev51188
+```
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,77 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import json
+import pytz
+from datetime import datetime, time
+
+
+INIT_URL = "https://devman.org/api/challenges/solution_attempts/"
+
+
+def get_json_data(url, parameters=None):
+    error = None
+    init_page_json_data = None
+    init_page_data = None
+    try:
+        init_page_data = requests.get(
+            url, params=parameters,
+            timeout=10).content.decode("utf-8")
+    except requests.exceptions.Timeout:
+        error = "Connection timed out"
+    except requests.exceptions.HTTPError:
+        error = "Status code - " + init_page_data.status_code + " recieved"
+    except requests.exceptions.ConnectionError:
+        error = "Could not connect to API"
+
+    if error is not None:
+        return None, error
+
+    try:
+        init_page_json_data = json.loads(init_page_data)
+    except json.decoder.JSONDecodeError:
+        error = "Can not load JSON"
+    return init_page_json_data, error
+
+
+def load_users_from_pages():
+    json_data, error = get_json_data(INIT_URL)
+    if error is not None:
+        raise ValueError(error)
+    page_count = json_data["number_of_pages"]
+    for page in range(1, page_count+1):
+        json_data, error = get_json_data(INIT_URL, {"page": page})
+        if error is not None:
+            print("Error: \"{}\" occured on page {}".format(error, page))
+            continue
+        for record in json_data["records"]:
+            yield record
+    return None
+
 
 def get_midnighters():
-    pass
+    midnighters = []
+    for user in load_users_from_pages():
+        user_tz = pytz.timezone(user["timezone"])
+        dt = datetime.fromtimestamp(user["timestamp"], user_tz)
+        if is_midnighter(dt.time()):
+            midnighters.append(user["username"])
+    if not midnighters:
+        return None
+    return set(midnighters)
 
-if __name__ == '__main__':
-  pass
+
+def is_midnighter(user_time):
+    night_start = time(0, 0, 0, 0)
+    night_end = time(7, 0, 0, 0)
+    if night_start < user_time < night_end:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    print("Searching midnighters...")
+    if get_midnighters() is not None:
+        print("Midnighters:")
+        for midnighter in get_midnighters():
+            print(midnighter)
+    else:
+        print("Could not get midnighters")
