8c879c1f86e65613d278432de899f3cf8c55301b
v1leftw <dmitry.g.fake@gmail.com>
2018-11-08 20:46:22 +0300

    [+] simplified

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,10 +1,6 @@
 import requests
-import json
 import pytz
-from datetime import datetime, time
-
-
-INIT_URL = "https://devman.org/api/challenges/solution_attempts/"
+from datetime import datetime
 
 
 def get_api_data(url, parameters=None):
@@ -13,71 +9,62 @@ def get_api_data(url, parameters=None):
     try:
         init_page_data = requests.get(
             url, params=parameters,
-            timeout=10).content.decode("utf-8")
+            timeout=10).json()
     except requests.exceptions.Timeout:
         error = "Connection timed out"
     except requests.exceptions.HTTPError:
         error = "Status code - " + init_page_data.status_code + " received"
     except requests.exceptions.ConnectionError:
         error = "Could not connect to API"
+    except ValueError:
+        error = "Can not decode JSON-data"
     return init_page_data, error
 
 
-def get_json_data(api_data):
-    _api_data, error = api_data
-    if error is not None:
-        return None, error
-    init_page_json_data = None
-    try:
-        init_page_json_data = json.loads(_api_data)
-    except json.decoder.JSONDecodeError:
-        error = "Can not load JSON"
-    return init_page_json_data, error
-
-
 def load_users_from_pages():
-    json_data, error = get_json_data(get_api_data(INIT_URL))
+    base_url = "https://devman.org/api/challenges/solution_attempts/"
+    json_data, error = get_api_data(base_url)
     if error is not None:
         raise ValueError(error)
     page_count = json_data["number_of_pages"]
     for page in range(1, page_count+1):
-        json_data, error = get_json_data(get_api_data(
-            "http://192.168.0.1:1111",
-            {"page": page}
-        ))
+        json_data, error = get_api_data(base_url, {"page": page})
         if error is not None:
             print("Error: \"{}\" occurred on page {}".format(error, page))
             continue
-        for record in json_data["records"]:
+        for record in json_data.get("records"):
             yield record
     return None
 
 
-def get_midnighters():
+def get_midnighters(*users):
     midnighters = []
-    for user in load_users_from_pages():
-        user_tz = pytz.timezone(user["timezone"])
-        dt = datetime.fromtimestamp(user["timestamp"], user_tz)
-        if is_midnighter(dt.time()):
+    for user in users:
+        if check_user(user):
             midnighters.append(user["username"])
-    if not midnighters:
-        return None
     return set(midnighters)
 
 
+def check_user(user):
+    user_tz = pytz.timezone(user["timezone"])
+    dt = datetime.fromtimestamp(user["timestamp"], user_tz)
+    if is_midnighter(dt.hour):
+        return user
+
+
 def is_midnighter(user_time):
-    night_start = time(0, 0, 0, 0)
-    night_end = time(7, 0, 0, 0)
-    if night_start < user_time < night_end:
+    night_start_hour = 0
+    night_end_hour = 7
+    if night_start_hour <= user_time < night_end_hour:
         return True
     return False
 
 
 if __name__ == "__main__":
     print("Searching midnighters...")
-    if get_midnighters() is not None:
+    if load_users_from_pages():
         print("Midnighters:")
-        for midnighter in get_midnighters():
+        for midnighter in get_midnighters(*load_users_from_pages()):
             print(midnighter)
     else:
         print("Could not get midnighters")
